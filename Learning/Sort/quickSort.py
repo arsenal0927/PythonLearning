040119d318a1e4cd95c1e7ba6735936d4ddec4bb
ArsenalLevel3 <18751967189@163.com>
2017-06-05 22:50:02 +0800

    add Sort

---
+++ Learning/Sort/quickSort.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+'''
+通过一趟排序将要排序的数据分割成独立的两部分，
+其中一部分的所有数据都比另外一部分的所有数据都要小，然后再按此方法对这两部分数据分别进行快速排序，
+整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+'''
+
+def quick_sort(lists, left, right):
+    if left >= right:
+        return lists
+    key = lists[left]
+    low = left
+    high = right
+    while left < right:
+        while left < right and lists[right] >= key:
+            right -= 1
+        lists[left] = lists[right]
+        while left < right and lists[left] <= key:
+            left += 1
+        lists[left] = lists[left]
+    lists[right] = key
+    quick_sort(lists, low, left - 1)
+    quick_sort(lists, left + 1, high)
+    return lists
+
+lists = [54,26,93,17,77,31,44,55,20]
+quick_sort(lists)
+print(lists)
+
